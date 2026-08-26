9c461256d99adaa93e1a34295c5083722a35b33f
LJitendra05 <l.jitendrak05@gmail.com>
2026-08-26 10:53:10 +0530

    Updates

--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -4,7 +4,10 @@ from utils.config_loader import load_devices
 
 devices=load_devices()
 
-@pytest.mark.parametrize("device",devices)
+@pytest.mark.parametrize(
+        "device",
+        devices,
+        ids=[device["Name"] for device in devices])
 def test_host_connectivity(device):
     result=ping_host(device["host"])
     assert result is device["expected_reachable"]
